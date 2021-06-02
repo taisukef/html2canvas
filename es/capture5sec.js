42b813917d3c5354d7c465fe0cadf233371f3939
Taisuke Fukuno <fukuno@jig.jp>
2021-06-03 08:23:55 +0900

    fix size

--- es/capture5sec.js
+++ es/capture5sec.js
@@ -3,17 +3,28 @@ import { GIF } from "https://taisukef.github.io/gifencoder/GIF.js";
 import { downloadFile } from "https://js.sabae.cc/downloadFile.js";
 import { sleep } from "https://js.sabae.cc/sleep.js";
 
-const capture5sec = async () => {
-  const delay = 100;
+let capturing = false;
+
+const capture5sec = async (delay = 100) => {
+  if (capturing) {
+    console.log("already capturing...")
+    return;
+  }
+  console.log("start capturing");
+  capturing = true;
   const frames = [];
+  let w, h;
   for (let i = 0; i < 5000 / delay; i++) {
-    const img = await html2image(document.body);
-    console.log(img.width, img.height);
+    const img = await html2image(document.body, w, h);
+    w = w || img.width;
+    h = h || img.height;
     frames.push(img);
     await sleep(delay);
   }
   const gif = GIF.encode(frames, delay);
   downloadFile("screenshot.gif", gif);
+  capturing = false;
+  console.log("capturing finished");
 };
 
 export { capture5sec };
--- es/html2image.js
+++ es/html2image.js
@@ -1,9 +1,9 @@
 import { html2canvas } from "./html2canvas.js";
 
-const html2image = async (component) => {
+const html2image = async (component, w, h) => {
   const canvas = await html2canvas(component);
   const ctx = canvas.getContext("2d");
-  const img = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const img = ctx.getImageData(0, 0, w || canvas.width, h || canvas.height);
   return img;
 };
 
